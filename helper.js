52db6826d71ec0347a81bba9fc7b690c025448dd
hz-tyfoon <md.tifun@gmail.com>
2021-05-25 14:47:20 +0600

    updated helper

--- helper.js
+++ helper.js
@@ -6,26 +6,22 @@ export const parseGradientColor = (gradientColor) => {
     radialShape = "ellipse",
     radialX = 50,
     radialY = 50;
-
   const [colorOne, colorTwo] = gradientColor.match(
-    /\#[a-f\d]{6}|rgba?\([\d\,\.]{3,16}\)/gi
-  );
-
-  const [gradientType] = gradientColor.match(/\w{6}(?=-)/i);
-
+    /\#[a-f\d]{3,8}|rgba?\([\d\,\.]{3,16}\)/gi
+  ) || ["#0000", "#0000"];
+  const [gradientType] = gradientColor.match(/\w{6}(?=-)/i) || ["linear"];
   if (gradientType == "linear") {
     angle = gradientColor.match(/\d{1,3}(?=deg)/i)[0];
     [colorOnePosition, colorTwoPosition] = gradientColor.match(
       /\d{1,3}(?=\%)/gi
     ) || [0, 100];
   } else {
-    radialShape = gradientColor.match(/\w+(?= at)/i)[0];
-    radialX = gradientColor.match(/(?<=at )\d{1,3}/i)[0];
-    radialY = gradientColor.match(/(?<=% )\d{1,3}/i)[0];
-    colorOnePosition = gradientColor.match(/\d{1,3}(?=% ,)/gi)[0];
-    colorTwoPosition = gradientColor.match(/\d{1,3}(?=%\))/gi)[0];
+    radialShape = (gradientColor.match(/\w+(?= at)/i) || [])[0];
+    radialX = (gradientColor.match(/(?<=at )\d{1,3}/i) || [])[0];
+    radialY = (gradientColor.match(/(?<=% )\d{1,3}/i) || [])[0];
+    colorOnePosition = (gradientColor.match(/\d{1,3}(?=% ,)/gi) || [])[0];
+    colorTwoPosition = (gradientColor.match(/\d{1,3}(?=%\))/gi) || [])[0];
   }
-
   return {
     gradientType,
     angle: parseInt(angle),
